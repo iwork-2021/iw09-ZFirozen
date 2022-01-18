8b1f0ba5d438cf6c9a493a852a81e53338b2443d
ZFirozen <zfirozen@ZFirozendeMac.local>
2022-01-18 12:59:31 +0800

    Try to compile on another Apple device.

---
+++ TinyToyTank/TinyToyTank/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  TinyToyTank
+//
+//  Created by ZFirozen on 2022/1/18.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var arView: ARView!
+    var tankAnchor : TinyToyTank._TinyToyTank?
+    var isActionPlaying: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        tankAnchor = try! TinyToyTank.load_TinyToyTank()
+        tankAnchor?.turret?.setParent(tankAnchor!.tank, preservingWorldTransform: true)
+        tankAnchor?.actions.actionComplete.onAction = { _ in
+            self.isActionPlaying = false
+        }
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(tankAnchor!)
+    }
+    
+    @IBAction func cannonFirePressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.cannonFire.post()
+    }
+    
+    @IBAction func turretRightPressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.turretRight.post()
+    }
+    
+    @IBAction func turretLeftPressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.turretLeft.post()
+    }
+    
+    @IBAction func tankForwardPressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.tankForward.post()
+    }
+    
+    @IBAction func tankRightPressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.tankRight.post()
+    }
+    
+    @IBAction func tankLeftPressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.tankLeft.post()
+    }
+    
+}
